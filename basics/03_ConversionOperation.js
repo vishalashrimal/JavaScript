1a48befe518356d388c124624e37fbd2d4b15d2b
Vishal Shrimal <127679432+vishalashrimal@users.noreply.github.com>
2024-10-06 08:01:59 +0000

    Conversion to Datatype

---
+++ basics/03_ConversionOperation.js
@@ -0,0 +1,39 @@
+let score = "23vishal"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+/* 
+"33" => 33 easily converted
+
+"33abc" =>NaN- Not a Number
+
+true => 1; false => 0
+*/
+
+ let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//  console.log(booleanIsLoggedIn);
+ 
+// Notes : 1 => true; 0 => false
+
+// "" => false
+ // "hitesh" => true
+
+
+let floatNumber= 3.14
+console.log(typeof floatNumber);
+
+let stringNumber = String(floatNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
