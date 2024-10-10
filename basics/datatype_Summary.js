91b6291c633ae9beaeb35dcea0998a0a4134fc82
Vishal Shrimal <127679432+vishalashrimal@users.noreply.github.com>
2024-10-10 02:15:52 +0000

    DataTypes Summary

---
+++ basics/datatype_Summary.js
@@ -0,0 +1,38 @@
+// Primitive 
+
+// 7 Types :- String, Number, null, undefined, boolean, bigInt, Symbol
+
+const score = 80
+const scoreValue = 80.10
+
+const isLoggedIn = false
+
+const outsideTemp = null 
+
+// let userEmail; undefined
+let userEmail = undefined;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+const bigNumber = 223221243567443556n
+
+
+// Reference(Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = [ "ShaktiMan", "Nagraaj", "Doga"];
+
+let obj ={
+    name : "Vishal",
+    age : 21,
+}
+
+let myFunction = function(){
+    console.log("Hello WOrld !");
+}
+
+
+console.log(typeof bigNumber)
