8eb6f254052c2a66bb10b9d30e0f8a92d610fc01
Vishal Shrimal <127679432+vishalashrimal@users.noreply.github.com>
2024-10-11 02:44:52 +0000

    Number And Math

---
+++ basics/06_number_Math.js
@@ -0,0 +1,38 @@
+const score = 100
+// console.log(score);
+
+const anotherScore = new Number(100)
+// console.log(anotherScore);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+const otherNumber = 223.8966
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.min(7,3,2,8,1));
+// console.log(Math.max(7,3,2,8,1));
+
+// console.log(Math.round(3.6));
+// console.log(Math.floor(3.8))
+// console.log(Math.ceil(3.2))
+
+console.log(Math.random());
+console.log(Math.round((Math.random()*6) +1)); //Mostly Used In Dice 1-6
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
