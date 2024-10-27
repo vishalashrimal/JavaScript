4471714f0a57a8ed7332ed98b10428f317f052f7
Vishal Shrimal <127679432+vishalashrimal@users.noreply.github.com>
2024-10-27 09:23:27 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,4 +22,37 @@ function calculator(num1, num2){
 function loginUserMessage(username){
     return`${username} just Logged in`;
 }
-console.log(loginUserMessage("Vishal"));
+// console.log(loginUserMessage("Vishal"));
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(100,200,300,456,252));
+
+const user={
+    username : "Vishal",
+    price : 999
+}
+
+function handleObject(anyObject){
+    // console.log(`username is ${anyObject.username} and price is ${anyObject.price}`); 
+}
+
+// handleObject(user);
+handleObject({
+    username : "Akash",
+    price : 300
+})
+
+
+const myArray = [200,400,100,600]
+
+function returnSecondValue(myArr){
+    // return myArr;
+    return myArr[1];
+}
+
+// console.log(returnSecondValue(myArray));
+console.log(returnSecondValue([100,200,654,234]));
+
